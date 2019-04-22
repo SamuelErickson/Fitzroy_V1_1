04236ca64fb1907158e06929babee28d26f0fa42
Sam Erickson PC <eric4204@umn.edu>
2019-04-22 16:18:06 -0500

    trying pigpio

---
+++ testPigPio.py
@@ -0,0 +1,11 @@
+import pigpio
+from time import sleep  # Importing sleep from time library
+
+pinNum = 21
+
+pi = pigpio.pi(21,pigpio.OUTPUT)
+pi.write(pinNum,1)
+sleep(2)
+pi.write(pinNum,0)
+sleep(2)
+pi.stop()
