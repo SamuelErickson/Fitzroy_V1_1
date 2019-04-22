4ed825fdba20a63f2712414497223187a746c817
Sam Erickson PC <eric4204@umn.edu>
2019-04-22 11:41:57 -0500

    Second commit, try PWM light

--- sunrise.py
+++ sunrise.py
@@ -12,14 +12,12 @@ GPIO.setmode(GPIO.BCM)  # We are using the BCM pin numbering
 GPIO.setup(led_pin, GPIO.OUT)  # Declaring pin 21 as output pin
 pwm = GPIO.PWM(led_pin, 100)  # Created a PWM object
 pwm.start(0)  # Started PWM at 0% duty cycle
-
-    while 1:  # Loop will run forever
-        for x in range(100):  # This Loop will run 100 times
-            pwm.ChangeDutyCycle(x)  # Change duty cycle
-            sleep(0.01)  # Delay of 10mS
-
-        for x in range(100, 0, -1):  # Loop will run 100 times; 100 to 0
-            pwm.ChangeDutyCycle(x)
-            sleep(0.01)
-#pwm.stop()  # Stop the PWM
+for x in range(100):  # This Loop will run 100 times
+    pwm.ChangeDutyCycle(x)  # Change duty cycle
+    sleep(0.01)  # Delay of 10mS
+sleep(5)
+#        for x in range(100, 0, -1):  # Loop will run 100 times; 100 to 0
+#           pwm.ChangeDutyCycle(x)
+#          sleep(0.01)
+pwm.stop()  # Stop the PWM
 GPIO.cleanup()  # Make all the output pins LOW
