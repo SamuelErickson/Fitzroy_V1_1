17adef0f07e40c39fe02d031bc8290205dc572d6
Sam Erickson PC <eric4204@umn.edu>
2019-04-22 16:25:46 -0500

    trying pigpio try pwm

--- testPigPio.py
+++ testPigPio.py
@@ -1,4 +1,5 @@
 import pigpio
+import numpy as np
 from time import sleep  # Importing sleep from time library
 
 pinNum = 21
@@ -8,11 +9,18 @@ pi.set_mode(pinNum,pigpio.OUTPUT)
 pi.write(pinNum,1)
 sleep(2)
 pi.write(pinNum,0)
-sleep(1)
-pi.set_PWM_dutycycle(pinNum,   192) # PWM off
-sleep(1)
-pi.set_PWM_dutycycle(pinNum,   128) # PWM off
-sleep(1)
-pi.set_PWM_dutycycle(pinNum,   64) # PWM off
 
+for i in np.linspace(0,255,256):
+    i = int(i)
+    pi.set_PWM_dutycycle(pinNum, i)  # PWM off
+    sleep(0.2)
+pi.write(pinNum,0)
+
+#pi.set_PWM_dutycycle(pinNum,   192) # PWM off
+#sleep(1)
+#pi.set_PWM_dutycycle(pinNum,   128) # PWM off
+#sleep(1)
+#pi.set_PWM_dutycycle(pinNum,   64) # PWM off
+
+#pi.write(pinNum,0)
 pi.stop()
