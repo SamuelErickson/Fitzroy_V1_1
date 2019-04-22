19a005a42af169698011eb3b2217c0dd2262fb50
Sam Erickson PC <eric4204@umn.edu>
2019-04-22 15:14:54 -0500

    change to 30 second change

--- sunrise.py
+++ sunrise.py
@@ -1,7 +1,7 @@
 
 """ Turn on light with PWM
 """
-
+import numpy as np
 
 import RPi.GPIO as GPIO  # Importing RPi library to use the GPIO pins
 from time import sleep  # Importing sleep from time library
@@ -9,7 +9,9 @@ from time import sleep  # Importing sleep from time library
 
 max_duty_cycle = 100 #final percent duty cycle 0 < dc < 100
 sunrise_period = 30 #seconds
-steptime = sunrise_period/max_duty_cycle #time between steps in sunrise/sunset
+steptime = 0.01
+n = int(sunrise_period/steptime)
+#steptime = sunrise_period/max_duty_cycle #time between steps in sunrise/sunset
 
 
 #Set pins
@@ -20,7 +22,7 @@ GPIO.setup(led_pin, GPIO.OUT)  # Declaring pin 21 as output pin
 pwm = GPIO.PWM(led_pin, 100)  # Created a PWM object with frequency of 100 hz
 pwm.start(0)  # Started PWM at 0% duty cycle
 try:
-    for x in range(max_duty_cycle):
+    for x in np.linspace(0,100,n):
         pwm.ChangeDutyCycle(x)  # Change duty cycle
         sleep(steptime)  # Delay of 10mS
     while True:
@@ -32,5 +34,6 @@ except KeyboardInterrupt:
 finally:
     pwm.stop()  # Stop the PWM
     GPIO.cleanup()  # Make all the output pins LOW
+    print("successfully clean up")
 
 
