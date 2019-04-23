4dc7a4dc0706f4dd1e6dacd9005890809cf63c74
Sam Erickson PC <eric4204@umn.edu>
2019-04-22 20:08:28 -0500

    try logging DHT

--- logTemp.py
+++ logTemp.py
@@ -2,10 +2,8 @@ if __name__ == "__main__":
     import time
     import pigpio
     import DHT22
-
-   # Intervals of about 2 seconds or less will eventually hang the DHT22.
-   INTERVAL=3
-
+    # Intervals of about 2 seconds or less will eventually hang the DHT22.
+    INTERVAL=3
     pi = pigpio.pi()
     s = DHT22.sensor(pi, 24)
     r = 0
