3b3abcb26f05a3494a6c377170745f0c7f4dde5f
Sam Erickson PC <eric4204@umn.edu>
2019-04-22 19:54:28 -0500

    try sinusoidal

--- sunrisesunset_demo.py
+++ sunrisesunset_demo.py
@@ -4,6 +4,16 @@ from time import sleep  # Importing sleep from time library
 from math import sin, pi as piConstant
 import sys
 
+"""
+Three optional arguments can be passed to this script in the command line
+1: sunriseDuration in seconds
+2: daytime in seconds
+3: frequency in hertz
+Example:
+python3 scriptName.py 20 5 300
+"""
+
+#
 if len(sys.argv) >= 4:
     freq = int(sys.argv[3])
     dayTime = int(sys.argv[2])
@@ -22,7 +32,7 @@ else:
     freq = 500
 
 incrementTime = sunriseDuration/256
-pinNum = 21
+pinNum = 23
 
 
 pi = pigpio.pi()
@@ -43,9 +53,7 @@ try:
         i = int(n * sin(i))
         pi.set_PWM_dutycycle(pinNum, i)  # PWM off
         sleep(incrementTime)
-finally:
-    print("activated finally")
+finally: #These lines will execute even if there is an exception error during the sunrise/sunset loops
     pi.write(pinNum, 0)
     pi.stop()
-    print("stop")
 
