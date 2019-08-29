f5623304b156a12845232980da799836bc448dc4
Sam Erickson PC <eric4204@umn.edu>
2019-08-29 16:37:04 -0500

    update printTempHum test

--- RaspberryPiVersion/tests/printTempHum.py
+++ RaspberryPiVersion/tests/printTempHum.py
@@ -2,12 +2,17 @@ if __name__ == "__main__":
     import time
     import datetime
     import pigpio
-    from RaspberryPiVersion import DHT22
+    import pandas as pd
+    from .. import DHT22
 
     # Intervals of about 2 seconds or less will eventually hang the DHT22.
+    df_config = pd.read_csv('../config.csv')
+
+    pinNum = int(df_config["DHT22_pin"].iloc[0])
+
     INTERVAL=3
     pi = pigpio.pi()
-    s = DHT22.sensor(pi, 24)
+    s = DHT22.sensor(pi, df_config)
     r = 0
 
 
