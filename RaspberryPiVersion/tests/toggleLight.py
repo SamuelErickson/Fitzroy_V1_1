c28096afe9b2bbd64d6cd1d945efc65ae8cfe26a
Sam Erickson PC <eric4204@umn.edu>
2020-01-10 15:35:14 -0600

    test

--- RaspberryPiVersion/tests/toggleLight.py
+++ RaspberryPiVersion/tests/toggleLight.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-pinNum = 21
+pinNum = 18
 #df_config = pd.read_csv('config.csv')
 #pinNum = int(df_config["light_pin"].iloc[0])
 GPIO.setup(pinNum,GPIO.OUT)
